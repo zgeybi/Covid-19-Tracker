3587716733e396f8c41f1b08a4d8e570cde18dc9
TimZgeybi <timzgeybi@gmail.com>
2021-02-03 09:23:04 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ class App extends React.Component {
         />
         <Chart data={data} country={country} />
         <Typography className="details" variant="h5">
-          Developed By Tim Zgeybi "\n"
-          On the 23rd of April 2020
+          Developed By Tim Zgeybi \n
+          Launched On the 23rd of April 2020
         </Typography>
         <Typography className="details">
           Instagram:
