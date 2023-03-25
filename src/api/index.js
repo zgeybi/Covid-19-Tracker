144229a13b97f1aa9d1eabaf992cc4ae548327c1
Tim Zgeybi <128317249+zgeybi@users.noreply.github.com>
2023-03-25 22:42:52 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://covid19.mathdro.id/api";
+/*const url = "https://covid19.mathdro.id/api";*/
 
 export const fetchData = async (country) => {
   let changeableUrl = url;
